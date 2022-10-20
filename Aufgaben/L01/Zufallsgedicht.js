2fd30eacf8bc5f04b8893ca5ff7fae20792199cf
JuliaBefus <shisara98@yahoo.de>
2022-10-20 13:05:12 +0200

    Aufgabe2

--- Aufgaben/L01/Zufallsgedicht.js
+++ Aufgaben/L01/Zufallsgedicht.js
@@ -5,20 +5,22 @@ var zufallsgedicht;
     Name: Julia Befus
     Matrikel: 271025
     Datum: 12.10.2022
-    Quellen: Aanya Khetarpal, Pia Giovannelli, Paula Jordans
+    Quellen: Aanya Khetarpal, Pia Giovannelli, Paula Jordans, Havva Kilic
     */
     let subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let prädikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objekte = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-    console.log(subjekte, prädikate, objekte);
+    console.log(subjekte);
+    console.log(prädikate);
+    console.log(objekte);
     for (let kamel = 6; kamel >= 1; kamel--) {
-        let letzterSatz = getVerse(subjekte, prädikate, objekte);
+        let letzterSatz = getVerse(subjekte, prädikate, objekte); //hier ist die Reihenfolge wichtig.
         console.log(letzterSatz);
     }
     function getVerse(_subjekte, _prädikate, _objekte) {
-        let zufallSubjekte = Math.floor(Math.random() * _subjekte.length);
-        let zufallPrädikate = Math.floor(Math.random() * _subjekte.length);
-        let zufallObjekte = Math.floor(Math.random() * _subjekte.length);
+        let zufallSubjekte = Math.floor(Math.random());
+        let zufallPrädikate = Math.floor(Math.random());
+        let zufallObjekte = Math.floor(Math.random());
         let verse = _subjekte[zufallSubjekte] + " " + _prädikate[zufallPrädikate] + " " + _objekte[zufallObjekte] + ".";
         _subjekte.splice(zufallSubjekte, 1);
         _prädikate.splice(zufallPrädikate, 1);
