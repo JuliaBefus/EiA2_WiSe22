9c914401db548cb6e7d34c7cd262119cb417ea6f
JuliaBefus <shisara98@yahoo.de>
2022-11-05 19:40:39 +0100

    Aufgabe4

--- Aufgaben/L04/data.ts
+++ Aufgaben/L04/data.ts
@@ -1,10 +1,10 @@
-/*
-Aufgabe: L04 Shopping List
-Name: Paula Jordans
-Matrikel: 271121
-Datum: 05.11.2022
-Quellen: Aanya Khetarpal, Pia Giovannelli, Julia Befus, Havva Kilic
-*/
+    /*
+    Aufgabe: 04 Shoppinglist
+    Name: Julia Befus
+    Matrikel: 271025
+    Datum: 05.11.2022
+    Quellen: Aanya Khetarpal, Pia Giovannelli, Paula Jordans, Havva Kilic und Bastian Aberle
+    */
 
 
 namespace shoppinglistA04 {
